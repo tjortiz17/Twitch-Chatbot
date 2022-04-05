f0812f14daefb015b0e4a652d03c676d9d2cb047
tjortiz17 <tjortiz17@gmail.com>
2022-04-05 13:01:24 -0700

    Get world record for chat queried game and respond with it in chat

--- app.js
+++ app.js
@@ -22,32 +22,6 @@ const client = new tmiJs.Client({
 //connects to specified channel
 client.connect().catch(console.error);
 
-//performs api call to get JSON object of the record run in this category and outputs it in chat
-async function getRecord(channel) {
-    const response = await fetch("https://www.speedrun.com/api/v1/leaderboards/sly1/category/all_keys?top=1");
-    let data = await response.json();
-    let recordString = buildRecord(data);
-    client.say(channel, recordString);
-    return data;
-}
-
-//builds and returns the string that will be returned by the bot on !wr
-//should know/get game name, category queried, and runner who got record
-function buildRecord(data){
-    let timeInSec = data.data.runs[0].run.times.primary_t;
-    let hours = Math.floor(timeInSec / 60 / 60);
-    let minutes = Math.floor(timeInSec / 60) - (hours * 60);
-    let seconds = timeInSec % 60;
-    
-    let minuteString = '';
-    let secondString = '';
-    if(minutes <= 9) minuteString = '0';
-    if(seconds <= 9) secondString = '0';
-
-    let record = 'World Record is ' + hours + ':' + minuteString + minutes + ':' + secondString + seconds;
-    return record;
-}
-
 //pass required params
 client.on('message', (channel, tags, message, self) => {
     if(self) return;
@@ -68,7 +42,7 @@ client.on('message', (channel, tags, message, self) => {
             client.say(channel, `@${tags.username} peepoHappy`);
             break;
         case '!wr':
-            getRecord(channel);
+            getRecord(channel, message.substring(5));
             break;
         default:
             //able to convert message to string to check for first word for other future commands like:
@@ -76,6 +50,48 @@ client.on('message', (channel, tags, message, self) => {
             if((myMessage.split(' ')[0]).toLowerCase() === '!test'){
                 client.say(channel, `Test message is obviously: "${myMessage.substring(6)}"`);
             }
+            else if((myMessage.split(' ')[0]).toLowerCase() === '!wr'){
+                getRecord(channel, myMessage.substring(4));
+            }
             break;
     }
-})
+});
+
+//performs api call to get JSON object of the record run in input game and category and outputs it in chat
+//default category will be any%
+async function getRecord(channel, message) {
+    //search for gameid on speedrun.com from message input
+    //let splitMessage = message.split('*');
+    let game = message.split(' ');
+    game = game.join('%20');
+    console.log(game);
+    let gameIdResponse = await fetch("https://www.speedrun.com/api/v1/games?name=" + game);
+    let gameIdData =  await gameIdResponse.json();
+    console.log(gameIdData);
+    let gameId = gameIdData.data[0].id;
+    console.log(gameId);
+    //find world record for input game
+    let response = await fetch("https://www.speedrun.com/api/v1/leaderboards/" + gameId + "/category/any?top=1");
+    let data = await response.json();
+    let recordString = buildRecord(data);
+    console.log(recordString);
+    client.say(channel, recordString);
+    return data;
+}
+
+//builds and returns the string that will be returned by the bot on !wr
+//should know/get game name, category queried, and runner who got record
+function buildRecord(data){
+    let timeInSec = data.data.runs[0].run.times.primary_t;
+    let hours = Math.floor(timeInSec / 60 / 60);
+    let minutes = Math.floor(timeInSec / 60) - (hours * 60);
+    let seconds = timeInSec % 60;
+    
+    let minuteString = '';
+    let secondString = '';
+    if(minutes <= 9) minuteString = '0';
+    if(seconds <= 9) secondString = '0';
+
+    let record = 'World Record is ' + hours + ':' + minuteString + minutes + ':' + secondString + seconds;
+    return record;
+}
